ff46af825b4b92a00cd2b03b3f966eb7092a15a8
PedroCaballero23 <pedro.s436646@cesurformacion.com>
2025-12-05 10:30:06 +0100

    cargar estilos(complemento)

--- Main.py
+++ Main.py
@@ -3,9 +3,21 @@ from PyQt5 import QtWidgets
 
 from Controller.Controller_main import MainWindow
 
+def cargar_estilos(app):
+    with open("EstilosVista.qss", "r") as f:
+        qss = f.read()
+        app.setStyleSheet(qss)
+        
+
+     
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("Vista.ui", self)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    cargar_estilos(app)
     ventana = MainWindow()
     ventana.show()
     sys.exit(app.exec_())
